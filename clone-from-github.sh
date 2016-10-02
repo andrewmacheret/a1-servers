0bbd73ef9b25aaed2068f9c84fce98987e138c08
Andrew Macheret <andrew.macheret@gmail.com>
2016-10-02 01:45:36 +0000

    Fix clone script [skip ci]

--- clone-from-github.sh
+++ clone-from-github.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-awk '$1 == "build:" {print $2}' docker-compose.yml | xargs -t -I{} git clone git@github.com:andrewmacheret/{}.git
+awk '$1 == "build:" {print $2}' docker-compose.yml | sed 's|/$||' | xargs -t -I{} git clone git@github.com:andrewmacheret/{}.git
