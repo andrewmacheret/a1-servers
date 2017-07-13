5db854cf7367c4625167b7d89eb9810cb5e8a976
Andrew Macheret <andrew.macheret@gmail.com>
2017-07-12 18:07:55 -0700

    Redo clone-servers.sh, fix letsencrypt issues, add caching volume to vta-tracker, remove remote-apis, and minor fix in restart-service.sh

--- clone-servers.sh
+++ clone-servers.sh
@@ -1,4 +1,36 @@
 #!/usr/bin/env bash
 set -e
 
-grep build docker-compose.yml  | grep -v '#' | awk '{print $2}' | sed 's:/::' | xargs -I{} git clone https://github.com/andrewmacheret/{}.git
+# prereqs:
+#  yum install jq
+#  yum install python-yaml
+
+yaml_to_json() {
+  python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout)'
+}
+
+BUILD_CONTEXTS="$(
+  cat docker-compose.yml |
+    yaml_to_json |
+    jq -r '.services[] | select(.build != null) | (.build, .build.context? | select(type == "string")) + "\t" + .image' |
+    sort
+)"
+
+echo "(Re)cloning build contexts:"
+echo "$BUILD_CONTEXTS" | column -t
+
+IFS=$'\n'
+for BUILD_CONTEXT in $BUILD_CONTEXTS; do
+  BUILD="$( echo "$BUILD_CONTEXT" | cut -f1 | xargs -n1 basename )"
+  IMAGE="$( echo "$BUILD_CONTEXT" | cut -f2 )"
+  echo
+  echo "BUILD=$BUILD"
+  echo "IMAGE=$IMAGE"
+  echo "..."
+
+  echo "rm -f '$BUILD' ..."
+  rm -rf "$BUILD"
+  echo "git clone 'git@github.com:${IMAGE}.git' '$BUILD'"
+  git clone "git@github.com:${IMAGE}.git" "$BUILD"
+done
+
